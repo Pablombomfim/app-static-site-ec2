4fdbbcbcf97f0a52aff6ccef76dee930e4a2f819
Pablo Bomfim <pablo.mb3107@gmail.com>
2023-08-30 22:13:00 -0300

    teste 36

--- terraform/modules/compu/compu.tf
+++ terraform/modules/compu/compu.tf
@@ -8,8 +8,13 @@ data "template_file" "user_data" {
 resource "aws_instance" "intancia" {
     ami                    = "ami-02e136e904f3da870"
     instance_type          = "t2.micro"
-    name               = "${var.name}"
     subnet_id              = "${var.vpc_sn_public_id}"
     vpc_security_group_ids = ["${var.vpc_sg_pub_id}"]
     user_data              = "${base64encode(data.template_file.user_data.rendered)}"
+
+  tags = {
+    Name = var.name
+ # Insira o nome da instância de sua preferência.
+  }
 }
+
