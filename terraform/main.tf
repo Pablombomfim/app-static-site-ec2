aab9ded92f8db07e6ab01f464cd1cbaf10536cd4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 21:32:18 -0300

    teste de segurança skip problemas medium

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "rede" {
 
 module "compu" {
   source           = "./modules/compu"
-  tipo_da_instacia = "t4.large"
+  tipo_da_instacia = "t3.large"
   name             = var.name
   vpc_sn_public_id = module.rede.vpc_sn_public_id
   vpc_sg_pub_id    = module.rede.vpc_sg_pub_id
