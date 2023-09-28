c534c3553ad766cd24a5e94e6a6f5cb67f4eee0b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-27 21:12:03 -0300

    teste de segurança

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 module "rede" {
   source   = "./modules/rede"
-  vpc_cidr = "${var.vpc_cidr}"
+  vpc_cidr = var.vpc_cidr
 
 }
 
 module "compu" {
   source           = "./modules/compu"
-  name        = "${var.name}"
-  vpc_sn_public_id = "${module.rede.vpc_sn_public_id}"
-  vpc_sg_pub_id    = "${module.rede.vpc_sg_pub_id}"
-
+  tipo_da_instacia = "t3.large"
+  name             = var.name
+  vpc_sn_public_id = module.rede.vpc_sn_public_id
+  vpc_sg_pub_id    = module.rede.vpc_sg_pub_id
 }
 
 #rapaz
--- terraform/modules/compu/vars.tf
+++ terraform/modules/compu/vars.tf
@@ -1,8 +1,15 @@
 variable "vpc_sn_public_id" {}
 
-variable "vpc_sg_pub_id"{}
+variable "vpc_sg_pub_id" {}
 
+variable "tipo_da_instacia" {
+  default = "t3.small"
+  validation {
+    condition     = substr(var.tipo_da_instacia, 0, 3) == "t3."
+    error_message = "Insira um tipo de instacia da geração t3"
+  }
+}
 variable "name" {
-    type = string
-    default = "Minha ec2"
+  type    = string
+  default = "Minha ec2"
 }
